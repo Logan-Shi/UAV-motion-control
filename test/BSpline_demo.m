16216affcf042f798316c3e7ef3e4878aaccf099
ida-zrt <ida_zrt@163.com>
2020-05-19 16:34:39 +0800

    add gif saving function

--- test/BSpline_demo.m
+++ test/BSpline_demo.m
@@ -6,6 +6,10 @@ P(:,1) = zeros(3,1);
 for i = 2:n
     P(:,i) = i*rand(3,1);
 end
+
+load waypts
+P = waypts;
+
 % function demo
 t = linspace(0,10,100);
 [p_u,v_u,a_u,j_u] = BSplineC(P,k,t);
@@ -20,7 +24,7 @@ plot3(P(1, :), P(2, :),P(3, :),...
                 'MarkerSize',6);
 line(P(1, :), P(2, :), P(3, :));
 plot3(p_u(1,:), p_u(2,:), p_u(3,:), 'Marker','.','LineStyle','-', 'Color',[.3 .6 .9]);
-quiver3(p_u(1,:), p_u(2,:), p_u(3,:),v_u(1,:), v_u(2,:), v_u(3,:))
+quiver3(p_u(1,:), p_u(2,:), p_u(3,:),v_u(1,:), v_u(2,:), v_u(3,:), 3)
 grid on;axis equal
 title('B-Spline Demo')
 xlabel('x');ylabel('y');zlabel('z')
